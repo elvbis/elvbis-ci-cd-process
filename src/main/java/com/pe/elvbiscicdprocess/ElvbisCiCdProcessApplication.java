9b9226577d3afb8d978248d78a4c3190336d7336
Elvis Humareda Romero <elvishromero@gmail.com>
2023-09-10 10:52:00 -0500

    first commit

---
+++ src/main/java/com/pe/elvbiscicdprocess/ElvbisCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.pe.elvbiscicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElvbisCiCdProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElvbisCiCdProcessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pe/elvbiscicdprocess/ElvbisCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.elvbiscicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElvbisCiCdProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
