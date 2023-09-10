6047c8edf9b98602f9dbadce1c95afb811b52bed
Elvis Humareda Romero <elvishromero@gmail.com>
2023-09-10 15:41:04 -0500

    deploy into heroku bring of docker images

--- src/test/java/com/pe/elvbiscicdprocess/ElvbisCiCdProcessApplicationTests.java
+++ src/test/java/com/pe/elvbiscicdprocess/ElvbisCiCdProcessApplicationTests.java
@@ -2,12 +2,15 @@ package com.pe.elvbiscicdprocess;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ElvbisCiCdProcessApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+@Test
+    public void mostrar_OK(){
+
+    Assert.isTrue(true,"holas");
+}
 
 }
